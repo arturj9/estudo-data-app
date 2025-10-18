cdd21da07cb2ebad8fb3de20b6e907d1311508b9
arturj9 <arturjardel9@gmail.com>
2025-10-18 12:57:00 -0300

    feat: Filtro de aprovados

--- app.py
+++ app.py
@@ -14,3 +14,17 @@ st.bar_chart(df[["Nota_1"]].set_index(df["Aluno"]))
 
 st.write("Nota 2")
 st.bar_chart(df[["Nota_2"]].set_index(df["Aluno"]))
+
+st.write("Média das Notas")
+df["Média"] = (df["Nota_1"] + df["Nota_2"]) / 2
+st.bar_chart(df[["Média"]].set_index(df["Aluno"]))
+
+st.write("Status de Aprovação")
+df["Aprovado"] = df["Média"] >= 7
+status_selecionado = st.selectbox("Selecione o status para filtrar os alunos:", options=["Todos", "Aprovados", "Reprovados"])
+if status_selecionado == "Aprovados":
+    st.dataframe(df[df["Aprovado"]])
+elif status_selecionado == "Reprovados":
+    st.dataframe(df[~df["Aprovado"]])
+else:
+    st.dataframe(df[["Aluno", "Média", "Aprovado"]])
